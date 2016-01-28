b93a9a2a8ca879148f0790c166bfa228378145d8
Pierre Caserta <pierrecaserta@tocs-MacBook-Pro-3.local>
2016-01-28 21:02:44 +1100

    add to janus

---
+++ add_to_janus.sh
@@ -0,0 +1,7 @@
+cd ~
+mkdir .janus
+cd .janus
+git clone https://github.com/vim-scripts/BufOnly.vim.git
+git clone https://github.com/NLKNguyen/papercolor-theme.git
+git clone https://github.com/jgdavey/tslime.vim.git
+git clone https://github.com/vim-airline/vim-airline.git
