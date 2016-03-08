ebe16d4b0f4b7d1d9bafa1b62a262bda1cfb4281
Pierre Caserta <pierrecaserta@tocs-MacBook-Pro-3.local>
2016-03-08 22:39:15 +1100

    vim-scala

--- add_to_janus.sh
+++ add_to_janus.sh
@@ -9,3 +9,4 @@ git clone https://github.com/jlanzarotta/bufexplorer.git
 git clone https://github.com/rking/ag.vim.git
 git clone https://github.com/nathanaelkane/vim-indent-guides.git
 git clone https://github.com/ensime/ensime-vim.git
+git clone https://github.com/derekwyatt/vim-scala.git
