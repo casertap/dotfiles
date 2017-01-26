d3a770b13a40e11b4e4ecd00d17477e5e0dd2dab
rolele <pierolele@gmail.com>
2017-01-27 08:56:57 +1100

    fix

--- add_to_janus.sh
+++ add_to_janus.sh
@@ -8,9 +8,10 @@ git clone https://github.com/vim-airline/vim-airline.git
 git clone https://github.com/jlanzarotta/bufexplorer.git
 git clone https://github.com/rking/ag.vim.git
 git clone https://github.com/nathanaelkane/vim-indent-guides.git
-git clone https://github.com/ensime/ensime-vim.git
-git clone https://github.com/derekwyatt/vim-scala.git
+#git clone https://github.com/ensime/ensime-vim.git
+#git clone https://github.com/derekwyatt/vim-scala.git
 git clone https://github.com/kien/rainbow_parentheses.vim.git
-git clone https://github.com/mikelue/vim-maven-plugin.git
+#git clone https://github.com/mikelue/vim-maven-plugin.git
 git clone git://github.com/jpalardy/vim-slime.git
 git clone git://github.com/will133/vim-dirdiff
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
